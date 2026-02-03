e34558b0992d968d8ce00af0312f33d4e1453599
Jyotish Pal <paljyotish36@gmail.com>
2026-02-03 11:49:39 +0530

    Added cors  URL

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const cors = require("cors");
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://graviton-frontend.vercel.app/"
+  ],
+  credentials: true
+}));
+
 require("dotenv").config();
 
 const connectDB = require("./config/db");
